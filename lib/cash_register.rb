219ca8808468d1ba0cf3b2e5cb10a513ae6ae400
metrio <dlima.nyit@gmail.com>
2020-10-09 21:55:37 -0400

    message

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
 
+    attr_accessor :total, :discount, :items, :last_transaction
+
+    def initialize(discount = 0)
+        @discount = discount
+        @items = []
+        @total = 0
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.total += (price * quantity)
+            quantity.times do 
+             items << title
+        end
+        self.last_transaction = quantity * price
+    end
+
+    def apply_discount
+       if discount != 0
+        self.total = (total * (100 - discount.to_f ) * 0.01).to_i
+        "After the discount, the total comes to $#{total}."
+       else
+        "There is no discount to apply."
+       end
+    end
+
+    def void_last_transaction
+        self.total = self.total - last_transaction 
+     end
+end
